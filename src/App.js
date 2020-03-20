ce613d830eda5114232a4af678696d53da2b3d86
tataqt <tata_98@ukr.net>
2020-03-20 13:42:41 +0200

    start redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+import FetchedPost from './components/FetchedPost';
 
 function App() {
   return (
-    <div className="container">
-    <h1>React</h1>
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">
+          <PostForm />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h2>Cинхронные посты</h2>
+          <Posts posts={[1, 2, 3]} />
+        </div>
+        <div className="col">
+          <h2>Асинхронные посты</h2>
+          <FetchedPost posts={[]} />
+        </div>
+      </div>
     </div>
   );
 }
