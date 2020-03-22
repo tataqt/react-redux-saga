07576496d232efd628ce2b1ce63ae6c88a2e06fa
tataqt <tata_98@ukr.net>
2020-03-22 14:28:58 +0200

    add Middleware

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from "./App";
 import { rootReducer } from "./redux/rootReducer";
+import { forbiddenWordsMiddleware } from "./redux/middleware";
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(
+      thunk,
+      forbiddenWordsMiddleware
+    ),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
